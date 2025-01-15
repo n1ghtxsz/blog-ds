e0f272f833f8bbda2fdf81baa8ad9a83720f0dfe
night <luissiqueira760@gmail.com>
2025-01-15 12:48:21 -0300

    Responsividade melhor, posts adicionados e padrao de cores.

--- src/scripts/common/item-container.js
+++ src/scripts/common/item-container.js
@@ -2,7 +2,7 @@ let contador = document.getElementById('item-container')
 let semitems = document.getElementById('no-items')
 
 if (contador.children.length === 0) {
-    semitems.innerHTML = `<h1 class="fs-5" >Este blog ainda não possui nenhuma postagem</h1>`;
+    semitems.innerHTML = `<h1 class="fs-5" >Este ano ainda não possui nenhuma postagem</h1>`;
 } else {
     semitems.innerHTML = ''; // Limpa a mensagem caso haja itens
 }
